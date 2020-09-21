bd7c389a3298ec20e840a5dfc074abc0b501dcb8
Werner Viljoen <ViljoenW@ukzn.ac.za>
2020-09-22 01:08:44 +0200

    Local db Initiated

---
+++ Migrations/20200921230002_initLocal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EZPayroll.MVC.Migrations
+{
+    public partial class initLocal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
